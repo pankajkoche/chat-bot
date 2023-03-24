49e573e83e77b431276c5da6551292f177d3a4f1
pankajkoche <pankajkoche3399@gmail.com>
2023-03-24 12:25:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React, { createContext, useState } from 'react';
+import ChatBot from './component/ChatBot';
+import Header from './component/Header';
+import {Route,Routes, Navigate} from 'react-router-dom'
+import Login from './component/Login';
+import Register from './component/Register';
+export const UserContext = createContext(null);
 function App() {
+  const [user, setUser] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className="mb-10">
+
+        <Header />
+        </div>
+        <div className="mt-5">
+        
+          <Routes>
+            <Route  exact path='/' element={user ? <ChatBot/>:<Navigate to="/login" /> }/>
+            <Route path='/login' element={user ?<ChatBot/>:<Login />}/>
+            <Route path='/register' element={ <Register />}/>
+          </Routes>
+        
+        
+        </div>
+      </UserContext.Provider>
+      
     </div>
   );
 }
